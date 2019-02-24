f8d42286c12d4e0b0765f3a12871493961a830a5
Michael R Campitiello <michaelrcampi@gmail.com>
2019-02-24 09:27:13 -0800

    2nd service worker commit

--- sw.js
+++ sw.js
@@ -28,9 +28,9 @@ let urlsToCache = [
     Activating Service Worker
 */
 
-self.addEventListener('activate', (event)=> {
+self.addEventListener('activate', (event)=>{
     event.waitUntil(
-        caches.keys().then((cacheNames)=> {
+        caches.keys().then((cacheNames)=>{
             return Promise.all(
                 cacheNames.filter((cacheName)=>{
                     return cacheName.startsWith('restaurant-') &&
@@ -48,5 +48,9 @@ self.addEventListener('activate', (event)=> {
 */
 
 self.addEventListener('fetch', (event)=>{
-    
-})
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            return response || fetch(event.request);
+        })
+    );
+});
