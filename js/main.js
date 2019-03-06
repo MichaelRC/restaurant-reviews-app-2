6c0f24c962e6972140e93ed000c199622418559c
Michael R Campitiello <michaelrcampi@gmail.com>
2019-03-06 10:39:55 -0800

    fixed MapBox token issue by removing <> from string.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1IjoibWljaGFlbHJjIiwiYSI6ImNqc3hoZTNhYTAyOXo0M3I1dTV3cWdyejMifQ.tBQ1xrlLmcjnYikBEw3_eg>',
+    mapboxToken: 'pk.eyJ1IjoibWljaGFlbHJjIiwiYSI6ImNqc3hoZTNhYTAyOXo0M3I1dTV3cWdyejMifQ.tBQ1xrlLmcjnYikBEw3_eg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
